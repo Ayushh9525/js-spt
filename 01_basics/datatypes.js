f3a2ab4b971a636b24fbc6a5205c305720894882
Ayush Shrivastava <107338194+Ayushh9525@users.noreply.github.com>
2025-09-08 13:02:58 +0000

    datatypes and their types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS as a newer version
+
+
+let name = "ayush" //string
+let age = 18 //number
+let isLoggedIn = "true" //boolean
+
+
+//PRIMITVE DATA TYPES
+//number 
+//bigint
+//string = ""
+//boolean = true/fales
+//null = standalone value
+//symbol = unique
+//undefined
+
+//NON PRIMITIVE DATA TYPES
+//object
+
+console.log(typeof age);
+console.log(typeof null)
