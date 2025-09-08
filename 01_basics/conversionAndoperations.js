04e378d52471d96ed07bb3f06af13e7db51fc8b7
Ayush Shrivastava <107338194+Ayushh9525@users.noreply.github.com>
2025-09-08 18:40:46 +0000

    converisionsAndOperations

--- 01_basics/conversionAndoperations.js
+++ 01_basics/conversionAndoperations.js
@@ -10,3 +10,46 @@ console.log(numvalue)
 let islogged = "ayush"
 let bool = Boolean(islogged)
 console.log(bool)
+
+
+let numm = 44
+let srtnum = String(numm)
+console.log(typeof srtnum)
+
+
+
+
+
+//******************* Operations  *******************/
+
+let vvalue = 3
+let negvalue = -vvalue
+
+console.log(negvalue)
+
+
+
+let str1 = "ayush"
+let str2 = "shrivastava"
+
+console.log(str1+ str2)
+
+
+console.log("1" + 2)
+console.log(2 + "1")
+console.log("2" + 1+ 2)
+console.log(2 +1 + "3")
+
+
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3 
+num1 = num2 = num3 = 2+2
+
+
+let gamecounter = 100
+++gamecounter //prefix
+gamecounter++ //postfix
+console.log(gamecounter)
+
