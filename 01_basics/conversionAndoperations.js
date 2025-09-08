a53caa0f007ed262ecfa0f79833afaad16f575b0
Ayush Shrivastava <107338194+Ayushh9525@users.noreply.github.com>
2025-09-08 18:28:14 +0000

    conversion

---
+++ 01_basics/conversionAndoperations.js
@@ -0,0 +1,12 @@
+let value = "asd"
+
+console.log(typeof value)
+
+let numvalue = Number(value)
+
+console.log(typeof numvalue)
+console.log(numvalue)
+
+let islogged = "ayush"
+let bool = Boolean(islogged)
+console.log(bool)
