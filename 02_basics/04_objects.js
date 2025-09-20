d444f8a9bd4bce69329b9025be34c539050b94fa
Ayush Shrivastava <107338194+Ayushh9525@users.noreply.github.com>
2025-09-20 05:44:36 +0000

    objects 04 completed

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -37,4 +37,17 @@ const obj3 = Object.assign({},obj1,obj2)
 // console.log(obj3)
 
 const obj4 = {...obj1,...obj2}
-console.log(obj4)
+// console.log(obj4)
+
+
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+
+console.log(tinderUser.hasOwnProperty('email'));
+
+
